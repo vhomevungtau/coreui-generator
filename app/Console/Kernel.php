7deb09e2dc46a06356119b1d5ad7cc3d69553951
vhomevungtau <vhomevungtau@gmail.com>
2021-10-16 19:22:13 +0700

    sms

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,11 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Server;
+use App\Models\Status;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +30,32 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            $users = User::all();
+
+            foreach ($users as $user) {
+                if (Carbon::parse($user->birthday)->isBirthday()) {
+                    // Send sms
+                    $data = Server::first()->attributesToArray();
+                    $url = $data['url'];
+                    $message = Status::find(210001)->first();
+                    $data['number'] = $user->phone;
+                    $data['message'] = sprintf($message, $user->name);
+                    Http::get($url, $data);
+
+                    // Zalo
+                    $dataZalo = [];
+                    $dataZalo['recipient']['user_id'] = env('ZALO_OA');
+                    $dataZalo['message']['text'] = $data['message'];
+
+                    Http::withHeaders([
+                        'access_token' => env('ZALO_TOKEN'),
+                        'Accept' => 'application/json',
+                    ])->post(env('ZALO_URL'), $dataZalo);
+                }
+            }
+        })->dailyAt('8:00');
     }
 
     /**
@@ -34,7 +65,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
